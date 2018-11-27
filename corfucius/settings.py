06e95d997224ae79227e4ee0a12ba2f88089a754
Corey Jones <cor@cmac-a.attlocal.net>
2018-11-26 18:59:56 -0600

    added settings for server

--- corfucius/settings.py
+++ corfucius/settings.py
@@ -132,3 +132,9 @@ STATICFILES_DIRS = [
 #Media Folder Settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+#for production server:
+try:
+    from .local_settings import *
+except ImportError:
+    pass
